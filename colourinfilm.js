4fc4d6b39007c683a4dd3297ccac9dd358aacf85
leilakhazma1 <leilakhazma7@gmail.com>
2024-03-07 15:51:43 +1100

    editing js

--- colourinfilm.js
+++ colourinfilm.js
@@ -1,19 +1,20 @@
-fetch('colourinfilm.json')
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-    const films = data.films;
-    const filmList = document.querySelector('.film-list');
-    films.forEach(film => {
-      const listItem = document.createElement('li');
-      listItem.textContent = film.name;
-      listItem.addEventListener('click', () => {
-        // Display film details or perform other actions
-        console.log(`Clicked on ${film.name}`);
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('colourinfilm.json')
+      .then(response => response.json())
+      .then(data => {
+        const filmList = document.querySelector('.film-list');
+        data.films.forEach(film => {
+          const listItem = document.createElement('li');
+          const link = document.createElement('a');
+          link.href = 'filmpage.html'; // Set the link destination
+          link.textContent = film.name; // Set the text content to the film name
+          listItem.appendChild(link);
+          filmList.appendChild(listItem);
+        });
+      })
+      .catch(error => {
+        console.error('Error:', error);
       });
-      filmList.appendChild(listItem);
-    });
-  })
-  .catch(error => {
-    console.error('Error:', error);
   });
+  
