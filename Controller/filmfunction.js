e40f0310745c9f19c5047a0239582c913c3d1ab4
leilakhazma1 <leilakhazma7@gmail.com>
2024-03-13 16:10:14 +1100

    typos

--- Controller/filmfunction.js
+++ Controller/filmfunction.js
@@ -1,11 +1,12 @@
 // Controller function to handle user actions
+const colourInFilm = require();
+
 function handleUserAction(action, params) {
     switch (action) {
     case "nextSlide": plusSlides(1);break;
     case "prevSlide": plusSlides(-1); break;
-    case "showSlide": currentSlide(params.slideNumber);
-    break; case "fetchFilmColors": fetchFilmColorsById(params.filmId);
-    break;
+    case "showSlide": currentSlide(params.slideNumber);break; 
+    case "fetchFilmColors": fetchFilmColorsById(params.filmId); break;
     default:
     break;
     }
